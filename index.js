b977f11394624e7040c0fd3734ac47fa068606fb
L. Cadet <85368055+Ludcad615@users.noreply.github.com>
2022-04-27 11:52:22 -0400

    commit tree 5c77d71a196c77e020525372d522469bd17e0c36

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file!
+
+//array= [1, 2, 3, 4,5 ]
+
+//const returnFirstTwoDrivers = drivers => drivers.slice(0, 2);
+
+function returnFirstTwoDrivers(arr){
+return arr.slice(0,2)
+}
+
+//const returnLastTwoDrivers = drivers => drivers.slice(-2);
+
+function returnLastTwoDrivers(arr){
+    return arr.slice(arr.length-2, arr.length)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+//const createFareMultiplier = num => fare => fare * num
+
+function createFareMultiplier(number){
+    return function(fare){
+        return fare * number
+    }
+}
+
+/*const fareDoubler = fare => {
+    let num = 2
+    return createFareMultiplier(num)(fare)
+}
+*/
+
+const fareDoubler = createFareMultiplier(2)
+
+/*const fareTripler = fare => {
+    let num = 3
+    return createFareMultiplier(num)(fare)
+}
+*/
+
+const fareTripler = createFareMultiplier(3)
+
+//const selectDifferentDrivers = (drivers, fn) => fn(drivers)
+
+function selectDifferentDrivers(list, aFunction){
+    return aFunction(list)
+}
